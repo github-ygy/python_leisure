88e5fb675a763c1681887d76a1e1582e1c74958f
ygy <ygyworkforit@163.com>
2017-09-20 00:37:00 +0800

    python  函数参数类型

--- py2/Function.py
+++ py2/Function.py
@@ -24,8 +24,25 @@
 # f.close()
 
 #有参数的函数
-def sum(num1,num2):
-    return num1 + num2
+# def sum(num1,num2):
+#     return num1 + num2
+#
+# # print(sum(1,2))
+# print(sum(num1=1,num2=2))
 
-# print(sum(1,2))
-print(sum(num1=1,num2=2))
+
+#传入多个参数
+# def mulParam(*args):
+#     print(args)
+#
+# mulParam(1,2,3,4,5,6)
+
+#传入字典 key value形式
+def mapParam(**kwargs):
+    print(kwargs)
+
+# mapParam(name="ygy",age='24')
+mapParam(**{'name':'ygy','age':24})
+
+
+
