1739ef79084e484610b950ef74511efa3e987379
ygy <ygyworkforit@163.com>
2017-09-20 00:15:51 +0800

    python  函数式编程

---
+++ py2/Function.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# author：ygy
+
+
+
+
+#声明函数
+# def func1():
+#     print("　test func1 ")
+#
+# def func2():
+#     print("　test func1 ")
+#
+# #调用函数
+# func1()
+# func2()
+
+#返回值的函数
+# def getFile():
+#     return open("new_test",'a',encoding="utf-8")
+#
+# f = getFile()
+# f.write(" haha \n")
+# f.close()
+
+#有参数的函数
+def sum(num1,num2):
+    return num1 + num2
+
+# print(sum(1,2))
+print(sum(num1=1,num2=2))
