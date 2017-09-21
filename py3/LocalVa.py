4a814c65b48526096c8562a8bc8dc02458316565
ygy <ygyworkforit@163.com>
2017-09-21 23:22:56 +0800

    python  py3 局部变量与全局变量

---
+++ py3/LocalVa.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# author：ygy
+
+msg = " global "
+
+##局部变量
+def getName(name):
+    age = 22
+    print(" name :",name)
+    print(" age :",age)
+
+
+# getName("ygy")
+
+
+#全局变量
+def getMsg():
+    global msg
+    msg = " local "
+    print(" msg : " ,msg)
+
+getMsg()
+
+print(msg)
