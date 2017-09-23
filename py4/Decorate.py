2c39b8c4141c9e97800c9d5845b5a9d3d2a47ec9
ygy <ygyworkforit@163.com>
2017-09-24 00:16:37 +0800

    python  py4 高阶函数与嵌套函数

---
+++ py4/Decorate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# author：ygy
+
+# 传入函数
+# def test1():
+#     print(" in the test1")
+#
+# def test2(func):
+#     print(" in the test2 ")
+#     print( func)
+#     return func
+#
+# test = test2(test1)
+# test()
+
+
+##嵌套函数
+# x =  0
+# def  grandparent():
+#     x = 1
+#     def parent():
+#         x = 2
+#         def child():
+#             x=3
+#             print(x)
+#         child()
+#     parent()
+#
+# grandparent()
