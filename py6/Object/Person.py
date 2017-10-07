beffb7273e243178d5f09c04acfe15bdc052e9e8
ygy <ygyworkforit@163.com>
2017-10-08 00:20:18 +0800

    python  py6  类的定义

--- py6/Object/Person.py
+++ py6/Object/Person.py
@@ -15,20 +15,35 @@ class Person:
     #析构方法
     #当对象销毁，或者释放时调用
     def __del__(self):
-        print(" self is destory")
+        print(" %s self is destory" %self.name)
 
 
     def printDesc(self):
         print(" name = %s , age = %d"%(self.name,self.age))
 
+#继承
+class Man(Person):
 
-p1 = Person("ygy",22)
-p2 = Person("anoy",21)
 
-p1.printDesc()
+    def __init__(self,name,age,desc):
+        # Person.__init__(self,name,age)
+        super(Man,self).__init__(name,age)
+        self.desc = desc
 
-del p1    #销毁p1对象，调用析构方法
 
-p2.printDesc()
+    def printDesc(self):
+        print(" this is man desc ")
 
-print(p2.state)
+# p1 = Person("ygy",22)
+# p2 = Person("anoy",21)
+#
+# p1.printDesc()
+#
+# del p1    #销毁p1对象，调用析构方法
+#
+# p2.printDesc()
+#
+# print(p2.state)
+
+m1 = Man("man",30,"this is man")
+m1.printDesc()
